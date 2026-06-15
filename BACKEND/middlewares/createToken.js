57e151f69508f522e30a9a45f4140486df7b8bc0
KHASIM <khasim2586@gmail.com>
2026-06-15 22:35:01 +0530

    small changes

--- BACKEND/middlewares/createToken.js
+++ BACKEND/middlewares/createToken.js
@@ -6,3 +6,5 @@ export function createToken(user){
     const signedToken=sign({id:user._id,email:user.email,name:user.name},process.env.JWT_SECRET,{expiresIn:process.env.expiresIn});
     return signedToken;
 }
+
+
